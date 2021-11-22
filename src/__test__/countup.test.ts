de9322997b1c58d62afcfb917de574bf46bfaf9c
elzup <guild0105@gmail.com>
2021-11-22 10:33:23 +0900

    add countup

---
+++ src/__test__/countup.test.ts
@@ -0,0 +1,12 @@
+import countup from '../countup'
+
+test('countup', () => {
+  expect(countup(['a', 'b', 'a', 1, 0, 0, 0])).toMatchInlineSnapshot(`
+Map {
+  "a" => 2,
+  "b" => 1,
+  1 => 1,
+  0 => 3,
+}
+`)
+})
---
+++ src/countup.ts
@@ -0,0 +1,10 @@
+const countup = (arr: (string | number)[]) => {
+  const m = new Map<string | number, number>()
+
+  arr.forEach((v) => {
+    m.set(v, (m.get(v) || 0) + 1)
+  })
+  return m
+}
+
+export default countup
