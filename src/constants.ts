aff87b156c17c433c16b4e244e7628b9c523dbf4
elzup <guild0105@gmail.com>
2021-11-23 21:48:06 +0900

    add chars

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,4 @@
-export const alphabets = 'abcdefghijklmnopqrstuvwxyz'
+export const charAlphabets = 'abcdefghijklmnopqrstuvwxyz'
+export const charNums = '0123456789'
+export const charAbc123 = charAlphabets + charNums
+export const charABCabc123 = charAlphabets.toUpperCase() + charAbc123
