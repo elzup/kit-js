c7c80e97b2a9d2b08bf7afddb5a182e338a01432
elzup <guild0105@gmail.com>
2022-01-08 00:35:30 +0900

    fix time format test

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,12 @@ module.exports = {
   moduleFileExtensions: ['ts', 'js'],
 
   transform: {
-    '^.+\\.(t|j)sx?$': '@swc/jest',
+    '^.+\\.(t|j)sx?$': [
+      '@swc/jest',
+      {
+        sourceMaps: true,
+      },
+    ],
   },
 
   testMatch: ['**/*.test.ts'],
--- src/__test__/times.test.ts
+++ src/__test__/times.test.ts
@@ -7,44 +7,37 @@ afterAll(jest.useRealTimers)
 
 const date = new Date(4836348428000)
 // "2123-04-05 06:07:08"
+const localDate = new Date('2123-04-05 06:07:08')
 
 test('times', () => {
-  jest.setSystemTime(date)
+  jest.setSystemTime(localDate)
 
-  expect(timesNow()).toMatchInlineSnapshot(`
-Object {
-  "day": 5,
-  "hour": 15,
-  "minute": 7,
-  "month": 4,
-  "second": 8,
-  "year": 2123,
-}
-`)
-
-  expect(times(date)).toMatchInlineSnapshot(`
-Object {
-  "day": 5,
-  "hour": 15,
-  "minute": 7,
-  "month": 4,
-  "second": 8,
-  "year": 2123,
-}
-`)
+  expect(times(localDate)).toMatchInlineSnapshot(`
+    Object {
+      "day": 5,
+      "hour": 6,
+      "minute": 7,
+      "month": 4,
+      "second": 8,
+      "year": 2123,
+    }
+  `)
 })
 
 test('formatTime', () => {
-  expect(formatTime(date)).toMatchInlineSnapshot(`"2123-04-05 15:07:08"`)
-  expect(formatYmd(date)).toMatchInlineSnapshot(`"2123-04-05"`)
-  expect(formatHms(date)).toMatchInlineSnapshot(`"15:07:08"`)
+  jest.setSystemTime(localDate)
+  expect(formatTime(localDate)).toMatchInlineSnapshot(`"2123-04-05 06:07:08"`)
+  expect(formatYmd(localDate)).toMatchInlineSnapshot(`"2123-04-05"`)
+  expect(formatHms(localDate)).toMatchInlineSnapshot(`"06:07:08"`)
 })
 
 test('shiftDate', () => {
-  expect(shiftDate(date, 0)).toMatchInlineSnapshot(`2123-04-04T21:07:08.000Z`)
-  expect(shiftDate(date, -6)).toMatchInlineSnapshot(`2123-04-04T15:07:08.000Z`)
-  expect(shiftDate(date, 5)).toMatchInlineSnapshot(`2123-04-05T02:07:08.000Z`)
+  jest.setSystemTime(date)
+  expect(+shiftDate(date, 0)).toMatchInlineSnapshot(`4836316028000`)
+  expect(+shiftDate(date, -6)).toMatchInlineSnapshot(`4836294428000`)
+  expect(+shiftDate(date, 5)).toMatchInlineSnapshot(`4836334028000`)
 })
 test('jpDate', () => {
-  expect(jpDate(date)).toMatchInlineSnapshot(`2123-04-05T06:07:08.000Z`)
+  jest.setSystemTime(date)
+  expect(+jpDate(date)).toMatchInlineSnapshot(`4836348428000`)
 })
