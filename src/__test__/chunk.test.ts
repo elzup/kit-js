035a202431fef21250e4c2b13aa1b03b4c1763d7
elzup <guild0105@gmail.com>
2022-01-08 17:33:20 +0900

    add test case

--- src/__test__/chunk.test.ts
+++ src/__test__/chunk.test.ts
@@ -7,4 +7,13 @@ test('chunk', () => {
     [7, 8, 9],
     [10],
   ])
+
+  expect(chunk([1, 2], 1)).toMatchObject([[1], [2]])
+  expect(chunk([1, 2], 0)).toMatchObject([])
+  expect(chunk([1, 2, 3, 4], 2)).toMatchObject([
+    [1, 2],
+    [3, 4],
+  ])
+  expect(chunk([], 3)).toMatchObject([])
+  expect(chunk([1, 2], 3)).toMatchObject([[1, 2]])
 })
--- src/chunk.ts
+++ src/chunk.ts
@@ -1,4 +1,5 @@
 export const chunk = <T>(arr: T[], size: number): T[][] => {
+  if (size < 1) return []
   const chunks: T[][] = []
 
   for (let i = 0; i < arr.length; i += size) {
