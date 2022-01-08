b07da525a290fb8b90faf313e2c6b694e1b8e3b5
elzup <guild0105@gmail.com>
2022-01-08 15:10:23 +0900

    change test

--- src/__test__/times.test.ts
+++ src/__test__/times.test.ts
@@ -1,18 +1,17 @@
 import { formatHms, formatTime, formatYmd } from '../formatTime'
-
-import { times, timesNow, jpDate, shiftDate } from '../times'
+import { jpDate, shiftDate, times } from '../times'
 
 beforeAll(() => jest.useFakeTimers('modern'))
 afterAll(jest.useRealTimers)
 
-const date = new Date(4836348428000)
+const date = new Date(4836380828000)
 // "2123-04-05 06:07:08"
-const localDate = new Date('2123-04-05 06:07:08')
+const lDate = new Date('2123-04-05 06:07:08')
 
 test('times', () => {
-  jest.setSystemTime(localDate)
+  jest.setSystemTime(lDate)
 
-  expect(times(localDate)).toMatchInlineSnapshot(`
+  expect(times(lDate)).toMatchInlineSnapshot(`
     Object {
       "day": 5,
       "hour": 6,
@@ -25,17 +24,17 @@ test('times', () => {
 })
 
 test('formatTime', () => {
-  jest.setSystemTime(localDate)
-  expect(formatTime(localDate)).toMatchInlineSnapshot(`"2123-04-05 06:07:08"`)
-  expect(formatYmd(localDate)).toMatchInlineSnapshot(`"2123-04-05"`)
-  expect(formatHms(localDate)).toMatchInlineSnapshot(`"06:07:08"`)
+  jest.setSystemTime(lDate)
+  expect(formatTime(lDate)).toMatchInlineSnapshot(`"2123-04-05 06:07:08"`)
+  expect(formatYmd(lDate)).toMatchInlineSnapshot(`"2123-04-05"`)
+  expect(formatHms(lDate)).toMatchInlineSnapshot(`"06:07:08"`)
 })
 
 test('shiftDate', () => {
-  expect(shiftDate(date, 0)).toMatchInlineSnapshot(`2123-04-04T21:07:08.000Z`)
-  expect(shiftDate(date, -6)).toMatchInlineSnapshot(`2123-04-04T15:07:08.000Z`)
-  expect(shiftDate(date, 5)).toMatchInlineSnapshot(`2123-04-05T02:07:08.000Z`)
+  expect(shiftDate(date, 0)).toMatchInlineSnapshot(`2123-04-05T06:07:08.000Z`)
+  expect(shiftDate(date, -6)).toMatchInlineSnapshot(`2123-04-05T00:07:08.000Z`)
+  expect(shiftDate(date, 5)).toMatchInlineSnapshot(`2123-04-05T11:07:08.000Z`)
 })
 test('jpDate', () => {
-  expect(jpDate(date)).toMatchInlineSnapshot(`2123-04-05T06:07:08.000Z`)
+  expect(jpDate(date)).toMatchInlineSnapshot(`2123-04-05T15:07:08.000Z`)
 })
