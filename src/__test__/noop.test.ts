e280bb1b08d069c04695529f2ed231a182fca061
elzup <guild0105@gmail.com>
2022-01-11 15:25:30 +0900

    add tagNoop

--- src/__test__/noop.test.ts
+++ src/__test__/noop.test.ts
@@ -1,6 +1,14 @@
-import noop from '../noop'
+import noop, { tagNoop } from '../noop'
 
 test('noop', () => {
   expect(noop()).toMatchInlineSnapshot(`undefined`)
   expect(noop(1, 2, 'three')).toMatchInlineSnapshot(`undefined`)
+})
+
+test('tagNoop', () => {
+  expect(tagNoop`abc`).toMatchInlineSnapshot(`"abc"`)
+  const age = 20
+
+  expect(tagNoop`bob(${age})`).toMatchInlineSnapshot(`"bob(20)"`)
+  expect(tagNoop`0-${1}-2-${3}`).toMatchInlineSnapshot(`"0-1-2-3"`)
 })
--- src/noop.ts
+++ src/noop.ts
@@ -1,3 +1,6 @@
 const noop = (..._args: unknown[]) => {}
 
+export const tagNoop = (strs: TemplateStringsArray, ...exps: any[]) =>
+  strs.reduce((a, c, i) => a + c + `${exps[i] || ''}`, '')
+
 export default noop
