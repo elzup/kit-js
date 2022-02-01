002a70b1eb095debee80788b189d326c5737e5f3
elzup <guild0105@gmail.com>
2022-02-01 20:57:12 +0900

    add more comp

--- src/__test__/comps.test.ts
+++ src/__test__/comps.test.ts
@@ -9,3 +9,12 @@ test('comps', () => {
   expect(comps([])).toStrictEqual([])
   expect(comps([1])).toStrictEqual([])
 })
+
+test('comps more 2', () => {
+  expect(comps([1, 2, 3, 4, 5, 6], 3)).toStrictEqual([
+    [1, 2, 3],
+    [2, 3, 4],
+    [3, 4, 5],
+    [4, 5, 6],
+  ])
+})
--- src/comps.ts
+++ src/comps.ts
@@ -1,10 +1,16 @@
-export function comps<T>(a: T[]): [T, T][] {
+export function comps2<T>(a: T[]): [T, T][] {
   if (a.length < 2) return []
 
-  const b = [...a]
-
-  b.shift()
-  a.pop()
+  const [, ...b] = a
 
   return b.map((v, i) => [a[i], b[i]])
+}
+
+export function comps<T>(a: T[], n = 2): T[][] {
+  if (a.length < n) return []
+  if (n === 2) return comps2(a)
+
+  const b = a.slice(n - 1, a.length)
+
+  return b.map((v, i) => a.slice(i, i + n))
 }
