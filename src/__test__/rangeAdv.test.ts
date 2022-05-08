e4a16d75199f4f778c6b8ad5dbd02d85b050af01
elzup <guild0105@gmail.com>
2022-05-08 14:45:44 +0900

    to required rangeAdv end arg

--- src/__test__/rangeAdv.test.ts
+++ src/__test__/rangeAdv.test.ts
@@ -1,8 +1,6 @@
 import { rangeAdv } from '../index'
 
-test('range', () => {
-  expect(rangeAdv(0)).toStrictEqual([])
-  expect(rangeAdv(5)).toStrictEqual([0, 1, 2, 3, 4])
+test('rangeAdv', () => {
   expect(rangeAdv(1, 4)).toStrictEqual([1, 2, 3])
   expect(rangeAdv(4, 1)).toStrictEqual([4, 3, 2])
   expect(rangeAdv(0, 8, 2)).toStrictEqual([0, 2, 4, 6])
@@ -10,11 +8,7 @@ test('range', () => {
   expect(rangeAdv(3, -3, -1)).toStrictEqual([3, 2, 1, 0, -1, -2])
 })
 
-test('range arg error', () => {
-  expect(() => {
-    rangeAdv(-10)
-  }).toThrowErrorMatchingInlineSnapshot(`"start cannot be negative"`)
-
+test('rangeAdv arg error', () => {
   expect(() => {
     rangeAdv(10, 20, -1)
   }).toThrowErrorMatchingInlineSnapshot(`"step cannot be negative"`)
