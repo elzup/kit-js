408c005f94dd6e3d928034dabd62586d70aa09ae
elzup <guild0105@gmail.com>
2022-05-22 22:50:43 +0900

    update

--- src/permutation.ts
+++ src/permutation.ts
@@ -1,5 +1,6 @@
 // referenced by https://qiita.com/higuma/items/5af4e62bdf4df42ce673 thanks
-export const permutation = <T>(
+
+export const permutationBase = <T>(
   post: T[],
   n: number = post.length,
   pre: T[] = [],
@@ -12,7 +13,18 @@ export const permutation = <T>(
   for (let i = 0, len = post.length; i < len; ++i) {
     const rest = post.slice(0)
 
-    permutation(rest, n - 1, pre.concat(rest.splice(i, 1)), perm)
+    permutationBase(rest, n - 1, pre.concat(rest.splice(i, 1)), perm)
   }
+  return perm
+}
+
+export const permutation = <T>(
+  post: T[],
+  n: number = post.length,
+  pre: T[] = []
+) => {
+  const perm: T[][] = []
+
+  permutationBase(post, n, pre, perm)
   return perm
 }
