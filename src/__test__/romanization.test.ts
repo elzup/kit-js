3ea385f4d742b03f3c48c64db2ad2ea18478277e
elzup <guild0105@gmail.com>
2022-06-08 09:13:27 +0900

    add test

--- src/__test__/romanization.test.ts
+++ src/__test__/romanization.test.ts
@@ -1,7 +1,13 @@
 import { romanization } from '../romanization'
 
-test('romanization', () => {
-  expect(romanization('あきすての')).toBe('akisuteno')
-  expect(romanization('はみゆれを')).toBe('hamiyurewo')
-  expect(romanization('がじづべぽ')).toBe('gazidubepo')
+describe('romanization', () => {
+  it('basic', () => {
+    expect(romanization('あきすての')).toBe('akisuteno')
+    expect(romanization('はみゆれを')).toBe('hamiyurewo')
+    expect(romanization('がじづべぽ')).toBe('gazidubepo')
+  })
+
+  it.todo('skip option')
+  it.todo('stretch')
+  it.todo('xya')
 })
