e0e303960f14280dcee1c97590ec582591faf4ce
elzup <guild0105@gmail.com>
2022-07-24 00:56:48 +0900

    revert add nominal

--- src/radix.ts
+++ src/radix.ts
@@ -1,20 +1,15 @@
-import { nominal, Nominal } from 'nominal-types'
-
-export type Decimal = Nominal<'Decimal', number>
-export type Digit = Nominal<'Digit', number>
-
-export const decToRadix = (n: Decimal, base: number): Digit[] => {
+export const decToRadix = (n: number, base: number): number[] => {
   if (base < 0) throw new Error('base must be positive')
 
-  const digits: Digit[] = []
-  let nn: Decimal = n
+  const digits: number[] = []
+  let nn: number = n
 
   while (nn > 0) {
-    const d = nominal.make<Digit>(nn % base)
+    const d = nn % base
 
     digits.push(d)
 
-    nn = nominal.make<Decimal>(Math.floor(nn / base))
+    nn = Math.floor(nn / base)
   }
   return digits
 }
