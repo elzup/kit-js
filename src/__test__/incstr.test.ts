749738d8f0a801f8dd2bb4ea7698c7726d26b5f0
elzup <guild0105@gmail.com>
2022-07-24 23:42:39 +0900

    comp test

--- src/__test__/incstr.test.ts
+++ src/__test__/incstr.test.ts
@@ -1,22 +1,28 @@
 import { encodeDigits, incstr, parseDigits } from '../index'
 
+test('parseDigits', () => {
+  const abcLib = { a: 0, b: 1, c: 2, d: 3, e: 4 }
+
+  expect(parseDigits('abcee', abcLib)).toStrictEqual([0, 1, 2, 4, 4])
+  expect(parseDigits('', abcLib)).toMatchInlineSnapshot(`Array []`)
+})
+
+test('encodeDigits', () => {
+  expect(encodeDigits([3, 2, 1, 4, 5], 'abcdefg')).toStrictEqual('dcbef')
+  expect(encodeDigits([3, 2, 1, 4, 5, 100], 'abcdefg')).toStrictEqual('dcbef')
+})
+
 describe('incstr', () => {
-  it('parseDigits', () => {
-    const abcLib = { a: 0, b: 1, c: 2, d: 3, e: 4 }
-
-    expect(parseDigits('abcee', abcLib)).toStrictEqual([0, 1, 2, 4, 4])
-    expect(parseDigits('', abcLib)).toMatchInlineSnapshot(`Array []`)
-  })
-
-  it('encodeDigits', () => {
-    expect(encodeDigits([3, 2, 1, 4, 5], 'abcdefg')).toStrictEqual('dcbef')
-  })
-
   it('diff', () => {
     expect(incstr('100', 1, '0123')).toStrictEqual('101')
     expect(incstr('333', -1, '0123')).toStrictEqual('332')
     expect(incstr('aaa', 1, '0abc')).toStrictEqual('aab')
   })
+
+  it('default arg', () => {
+    expect(incstr('100')).toStrictEqual('101')
+  })
+
   it('empty', () => {
     expect(incstr('', 0, '0abc')).toBe('')
     expect(incstr('a', -1, '0abc')).toBe('')
