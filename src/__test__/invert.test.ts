38e692a15dcbc41f2dad24b8aa1b76a0e48b70ef
elzup <guild0105@gmail.com>
2022-07-24 18:27:36 +0900

    add after callback to invert

--- src/__test__/invert.test.ts
+++ src/__test__/invert.test.ts
@@ -1,31 +1,36 @@
+import { expectType } from 'tsd'
 import { invert, swap, swapKeyValue } from '../index'
 
 test('swap', () => {
   expect(swap(['a', 1])).toStrictEqual([1, 'a'])
 })
-test('invert', () => {
-  expect(invert({ a: 1, 2: 'b' })).toMatchInlineSnapshot(`
-    Object {
-      "1": "a",
-      "b": "2",
-    }
-  `)
-  expect(swapKeyValue({ a: 'alias' })).toMatchInlineSnapshot(`
-    Object {
-      "alias": "a",
-    }
-  `)
+describe('invert', () => {
+  it('basic', () => {
+    const res = invert({ a: 1, 2: 'b' })
 
-  expect(invert('ab')).toMatchInlineSnapshot(`
-    Object {
-      "a": "0",
-      "b": "1",
-    }
-  `)
-  expect(invert(['c', 'd'])).toMatchInlineSnapshot(`
-    Object {
-      "c": "0",
-      "d": "1",
-    }
-  `)
+    expect(res).toStrictEqual({
+      '1': 'a',
+      b: '2',
+    })
+    expectType<Record<string, 1 | 'b'>>(res)
+  })
+  it('alias', () => {
+    expect(swapKeyValue({ a: 'alias' })).toStrictEqual({ alias: 'a' })
+  })
+  it('string or array format', () => {
+    expect(invert('ab')).toStrictEqual({ a: '0', b: '1' })
+    expect(invert(['c', 'd'])).toStrictEqual({ c: '0', d: '1' })
+  })
+
+  it('after callback', () => {
+    const res = invert('ab', (v) => Number(v))
+
+    expectType<Record<string, number>>(res)
+    expect(res).toMatchInlineSnapshot(`
+      Object {
+        "a": 0,
+        "b": 1,
+      }
+    `)
+  })
 })
--- src/invert.ts
+++ src/invert.ts
@@ -1,12 +1,23 @@
 export const swap = <T, U>([k, v]: [T, U]): [U, T] => [v, k]
 
-type InvertFn = <T extends PropertyKey>(
-  obj: Record<string, T> | string[] | string
-) => Record<string, T>
+type Entryable<T> = string[] | string | Record<string, T>
 
-export const invert: InvertFn = (
-  obj
-  // after: (v: T) => T = (v) => v
-) => Object.fromEntries(Object.entries(obj).map(swap))
+type InvertFn = {
+  <T extends PropertyKey>(obj: Entryable<T>): Record<string, T>
+  <T extends PropertyKey, V>(
+    obj: Entryable<T>,
+    after: (v: string) => V
+  ): Record<string, V>
+}
+
+export const invert: InvertFn = <T extends PropertyKey, V = T>(
+  obj: Entryable<T>,
+  after?: (v: string) => V
+) =>
+  Object.fromEntries(
+    Object.entries(obj)
+      .map(swap)
+      .map(([k, v]) => [k, after ? after(v) : v])
+  )
 
 export const swapKeyValue = invert
