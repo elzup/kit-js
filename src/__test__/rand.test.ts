4bde288a789a806e71b55ac2f97167fefaa2a934
elzup <guild0105@gmail.com>
2022-08-14 20:27:10 +0900

    add rand test

--- src/__test__/rand.test.ts
+++ src/__test__/rand.test.ts
@@ -1,9 +1,125 @@
-describe('rand', () => {
-  beforeEach(() => {
-    jest.spyOn(global.Math, 'random').mockReturnValue(0.123456789)
+import {
+  randRangeV4 as randRange,
+  choiseV4 as choise,
+  sampleV4 as sample,
+  shuffleV4 as shuffle,
+  choiseSeed,
+  randRangeSeed,
+  sampleSeed,
+  shuffleSeed,
+} from '../index'
+
+describe('randnext', () => {
+  describe('use Math.random', () => {
+    beforeEach(() => {
+      jest
+        .spyOn(global.Math, 'random')
+        .mockReturnValueOnce(0.333)
+        .mockReturnValueOnce(0.111)
+        .mockReturnValueOnce(0.444)
+        .mockReturnValueOnce(0.555)
+        .mockReturnValueOnce(0.999)
+        .mockReturnValue(0.123456789)
+    })
+
+    afterEach(() => {
+      jest.spyOn(global.Math, 'random').mockRestore()
+    })
+    it('randRange', () => {
+      const v1 = randRange(10)
+      const v2 = randRange(20, 30)
+
+      expect(v1).toMatchInlineSnapshot(`3`)
+      expect(v2).toMatchInlineSnapshot(`21`)
+    })
+
+    it('shuffle', () => {
+      const res = shuffle([1, 2, 3, 4, 5])
+
+      expect(res).toStrictEqual([2, 1, 3, 4, 5])
+    })
+
+    it('sample', () => {
+      const res3 = sample([1, 2, 3, 4, 5], 3)
+
+      expect(res3).toStrictEqual([1, 2, 5])
+    })
+    it('choise', () => {
+      const res = choise([1, 2, 3, 4, 5])
+
+      expect(res).toMatchInlineSnapshot(`2`)
+    })
   })
 
-  afterEach(() => {
-    jest.spyOn(global.Math, 'random').mockRestore()
+  it('randRange', () => {
+    const v1 = randRange(10, () => 0.42)
+    const v2 = randRange(20, 30, () => 0.5)
+
+    expect(v1).toMatchInlineSnapshot(`4`)
+    expect(v2).toMatchInlineSnapshot(`25`)
+  })
+  it('shuffle', () => {
+    let i = 0
+    const res = shuffle([1, 2, 3, 4, 5], () => i--)
+
+    expect(res).toStrictEqual([5, 4, 3, 2, 1])
+  })
+
+  it('sample', () => {
+    const res3 = sample([1, 2, 3, 4, 5], 3, () => 0.5)
+
+    expect(res3).toStrictEqual([1, 3, 5])
+  })
+  it('choise', () => {
+    const res = choise([1, 2, 3, 4, 5], () => 0.3)
+
+    expect(res).toMatchInlineSnapshot(`2`)
+  })
+})
+
+describe('rand seed', () => {
+  it('randRangeSeed', () => {
+    const v1 = randRangeSeed(0, 10, 'a1')
+    const v2 = randRangeSeed(20, 30, 'a1')
+
+    expect(v1).toMatchInlineSnapshot(`3`)
+    expect(v2).toMatchInlineSnapshot(`23`)
+  })
+
+  it('shuffleSeed', () => {
+    const res = shuffleSeed([1, 2, 3, 4, 5], 'b')
+
+    expect(res).toMatchInlineSnapshot(`
+    Array [
+      3,
+      4,
+      2,
+      1,
+      5,
+    ]
+  `)
+  })
+
+  it('sampleSeed', () => {
+    const res1 = sampleSeed([1, 2, 3, 4, 5], 1, 'a')
+    const res3 = sampleSeed([1, 2, 3, 4, 5], 3, 'a')
+
+    expect(res1).toMatchInlineSnapshot(`
+    Array [
+      3,
+    ]
+  `)
+    expect(res3).toMatchInlineSnapshot(`
+    Array [
+      2,
+      3,
+      5,
+    ]
+  `)
+  })
+  it('choiseSeed', () => {
+    const res = choiseSeed([1, 2, 3, 4, 5], 'a')
+
+    expect(res).toMatchInlineSnapshot(`5`)
   })
 })
--- src/rand/randRange.ts
+++ src/rand/randRange.ts
@@ -1,17 +1,20 @@
-const randRangeHit = (v: number, min: number, max?: number) => {
-  const [low, high] = max === undefined ? [0, min] : [min, max]
+const randRangeHit = (low: number, high: number, v: number) => {
   const d = high - low
+
+  console.log(d)
+  console.log(v * d)
+  console.log(Math.floor(v * d))
 
   return low + Math.floor(v * d)
 }
 
 export const randRange = (
   min: number,
-  max?: number,
+  max: number | (() => number) = Math.random,
   rand = Math.random
 ): number => {
-  if (typeof max !== 'number') {
-    return randRange(0, min, rand)
+  if (typeof max === 'function') {
+    return randRange(0, min, max)
   }
   return randRangeHit(min, max, rand())
 }
