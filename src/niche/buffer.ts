65a67b3d1453b545cbbd13c67341eb4286dae166
elzup <guild0105@gmail.com>
2022-12-23 02:11:29 +0900

    add shift

--- src/niche/buffer.ts
+++ src/niche/buffer.ts
@@ -3,7 +3,7 @@ const encodeCodePointShift = (n: number, shift = 0) =>
 const decodeCodePointShift = (s: string, shift = 0) =>
   (s.codePointAt(0) ?? 0) - shift
 
-export const repartitionBits = (buf: Uint8Array): string => {
+export const repartitionBits = (buf: Uint8Array, shift = 0): string => {
   const bits = Array.from(buf)
     .map((b) => b.toString(2).padStart(8, '0'))
     .join('')
@@ -11,7 +11,7 @@ export const repartitionBits = (buf: Uint8Array): string => {
 
   return Array.from(b6s)
     .map((b) => b.padEnd(6, '0'))
-    .map((b) => encodeCodePointShift(parseInt(b, 2)))
+    .map((b) => encodeCodePointShift(parseInt(b, 2)), shift)
     .join('')
 }
 
@@ -26,3 +26,9 @@ export const unRepartitionBits = (str: string): Uint8Array => {
 
   return new Uint8Array(Array.from(u8s))
 }
+
+'⡀'.codePointAt(0) // 8bit 1~256
+
+new TextDecoder().decode(
+  Uint8Array.from([0b11101111, 0b10111111, 0b10111111, 0b10111111])
+)
