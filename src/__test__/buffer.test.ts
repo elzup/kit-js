f35a84c8a48236537b3886eba40c81268a7e41b7
elzup <guild0105@gmail.com>
2022-12-25 23:36:54 +0900

    update repatition

--- src/__test__/buffer.test.ts
+++ src/__test__/buffer.test.ts
@@ -5,8 +5,16 @@ describe('repartitionBits', () => {
     expect(
       repartitionBits(
         new Uint8Array([0b11111111, 0b00001111, 0b00110011, 0b01010101]),
-        'あ'.codePointAt(0)
+        6
       )
     ).toMatchInlineSnapshot(`"?0<3"`)
+  })
+})
+
+describe('unRRepartitionBits', () => {
+  it('basic', () => {
+    expect(
+      Buffer.from(unRepartitionBits('abcdefg', 6)).toString('hex')
+    ).toMatchInlineSnapshot(`"c38b1e4cb9b3"`)
   })
 })
