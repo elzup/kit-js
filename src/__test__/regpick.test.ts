127a5abdfe690bc50ab5cff76bb0a40505e547dd
elzup <guild0105@gmail.com>
2023-01-07 20:55:56 +0900

    add matcher

--- src/__test__/regpick.test.ts
+++ src/__test__/regpick.test.ts
@@ -1,12 +1,19 @@
-import { regpick } from '../index'
+import { regpick, regpickMatcher } from '../index'
+
+const binstr = (bs: boolean[]) => bs.map((v) => (v ? '1' : '0')).join('')
 
 describe('regpick', () => {
   it('fieldFn', () => {
-    const a = [0, 1, 0, 0, 0, 1, 0]
-
-    expect(regpick(a, Boolean, /10+1/)).toStrictEqual({
+    expect(regpick([0, 1, 0, 0, 0, 1, 0], Boolean, /10+1/)).toStrictEqual({
       matches: [1, 0, 0, 0, 1],
       prints: [false, true, true, true, true, true, false],
+    })
+
+    expect(
+      regpick(['a01', 'b01', 'b02', 'a02'], (s) => s.startsWith('b'), /1+/)
+    ).toStrictEqual({
+      matches: ['b01', 'b02'],
+      prints: [false, true, true, false],
     })
 
     expect(
@@ -25,5 +32,54 @@ describe('regpick', () => {
       matches: [1, 0, 1, 0, 1],
       prints: [false, true, true, true, true, true, false],
     })
+  })
+
+  it('matches', () => {
+    const b00101010 = [0, 0, 1, 0, 1, 0, 1, 0]
+    const b11000101 = [1, 1, 0, 0, 0, 1, 0, 1]
+    const { HEAD_0, HEAD_01, HEAD_1, HEAD_10 } = regpickMatcher
+    const { TAIL_0, TAIL_01, TAIL_1, TAIL_10, IN_010, IN_101 } = regpickMatcher
+
+    const {} = regpickMatcher
+
+    expect(binstr(regpick(b00101010, Boolean, HEAD_0).prints)).toStrictEqual(
+      '11000000'
+    )
+
+    expect(binstr(regpick(b00101010, Boolean, HEAD_01).prints)).toStrictEqual(
+      '11100000'
+    )
+
+    expect(binstr(regpick(b11000101, Boolean, HEAD_1).prints)).toStrictEqual(
+      '11000000'
+    )
+
+    expect(binstr(regpick(b11000101, Boolean, HEAD_10).prints)).toStrictEqual(
+      '11111000'
+    )
+
+    expect(binstr(regpick(b00101010, Boolean, TAIL_0).prints)).toStrictEqual(
+      '00000001'
+    )
+
+    expect(binstr(regpick(b11000101, Boolean, TAIL_01).prints)).toStrictEqual(
+      '00000011'
+    )
+
+    expect(binstr(regpick(b11000101, Boolean, TAIL_1).prints)).toStrictEqual(
+      '00000001'
+    )
+
+    expect(binstr(regpick(b00101010, Boolean, TAIL_10).prints)).toStrictEqual(
+      '00000011'
+    )
+
+    expect(binstr(regpick(b00101010, Boolean, IN_010).prints)).toStrictEqual(
+      '11111111'
+    )
+
+    expect(binstr(regpick(b00101010, Boolean, IN_101).prints)).toStrictEqual(
+      '00111110'
+    )
   })
 })
--- src/regpick.ts
+++ src/regpick.ts
@@ -6,13 +6,25 @@ type RegpickReturn<U> = {
   prints: Print[]
 }
 
+export const regpickMatcher = {
+  IN_101: /1.*1/,
+  IN_010: /0.*0/,
+  HEAD_0: /^0+/,
+  HEAD_01: /^0+1+/,
+  HEAD_1: /^1+/,
+  HEAD_10: /^1+0+/,
+  TAIL_0: /0+$/,
+  TAIL_1: /1+$/,
+  TAIL_01: /0+1+$/,
+  TAIL_10: /1+0+$/,
+}
+
 export const regpick = <U>(
   arr: U[],
   arg2: ((t: U) => boolean) | Plan[],
   r: RegExp
-): RegpickReturn<U> => {
-  return regpickCore(arr, typeof arg2 === 'function' ? arr.map(arg2) : arg2, r)
-}
+): RegpickReturn<U> =>
+  regpickCore(arr, typeof arg2 === 'function' ? arr.map(arg2) : arg2, r)
 
 const regpickCore = <U>(
   arr: U[],
@@ -24,13 +36,9 @@ const regpickCore = <U>(
   return { matches: arr.filter((_v, i) => prints[i]), prints }
 }
 
+const binstr = (bs: boolean[]) => bs.map((v) => (v ? '1' : '0')).join('')
 const planToPrint = (plans: Plan[], r: RegExp): Print[] => {
-  const blue = plans.map((v) => (v ? '1' : '0')).join('')
-
-  console.log({ blue })
-  const m = blue.match(r)
-
-  console.log(m)
+  const m = binstr(plans).match(r)
 
   if (m === null || m.index === undefined) return plans.map(() => false)
 
