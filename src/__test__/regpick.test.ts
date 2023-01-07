aabf6a13299860aaa23598729afa2e7be1141b22
elzup <guild0105@gmail.com>
2023-01-07 19:50:23 +0900

    add test

--- src/__test__/regpick.test.ts
+++ src/__test__/regpick.test.ts
@@ -1,7 +1,7 @@
 import { regpick } from '../index'
 
 describe('regpick', () => {
-  it('middle', () => {
+  it('fieldFn', () => {
     const a = [0, 1, 0, 0, 0, 1, 0]
 
     expect(regpick(a, Boolean, /10+1/)).toStrictEqual({
@@ -14,6 +14,16 @@ describe('regpick', () => {
     ).toStrictEqual({
       matches: [1, 0, 0, 1],
       prints: [false, true, true, true, true, false, false, false, false],
+    })
+  })
+
+  it('bools', () => {
+    const a = [0, 1, 0, 1, 0, 1, 0]
+    const preCalc = a.map(Boolean)
+
+    expect(regpick(a, preCalc, /1.*1/)).toStrictEqual({
+      matches: [1, 0, 1, 0, 1],
+      prints: [false, true, true, true, true, true, false],
     })
   })
 })
