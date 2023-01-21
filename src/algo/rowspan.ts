ac18a517168cd5e5795a99654059da217db824fd
elzup <guild0105@gmail.com>
2023-01-21 12:04:44 +0900

    refactor

--- src/algo/rowspan.ts
+++ src/algo/rowspan.ts
@@ -1,12 +1,18 @@
-export const rowspan = (cells: any[][]) => {
-  return cells.map((row, i) => {
-    if (i === 0) {
-      return Array.from({ length: row.length }, () => true)
-    }
-    const prevRow = cells[i - 1]
-    const l = row.findIndex((cell, j) => cell !== prevRow[j])
-    const v = l === -1 ? row.length + 1 : l
+import { windowed2 } from '../windowed'
 
-    return Array.from({ length: row.length }, (_v, i) => i >= v)
-  })
+const unOtherwise = (v: number) => (v === -1 ? Infinity : v)
+
+const diffPos = (row1: readonly any[], row2: readonly any[]) =>
+  row1.findIndex((v, j) => v !== row2[j])
+
+// (4, 2) => [false, false, true, true]
+const makeBoolRow = (length: number, truePos: number) =>
+  Array.from({ length }, (_v, i) => i >= truePos)
+
+export const rowspan = (cells: readonly any[][]) => {
+  if (cells.length === 0) return []
+  const length = cells[0]?.length ?? 0
+  const positions = windowed2(cells).map((v) => unOtherwise(diffPos(...v)))
+
+  return [0, ...positions].map((v) => makeBoolRow(length, v))
 }
