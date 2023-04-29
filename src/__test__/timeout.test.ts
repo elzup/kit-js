836743cc19faf01ca7c0d6c177fb5af12e940a76
elzup <guild0105@gmail.com>
2023-04-29 17:35:48 +0900

    refactor test

--- src/__test__/timeout.test.ts
+++ src/__test__/timeout.test.ts
@@ -1,4 +1,9 @@
-import { sleep, timeout } from '..'
+import { sleep, timeout, timeoutCover } from '..'
+
+async function heavyWork(msec: number) {
+  await sleep(msec)
+  return 'end'
+}
 
 describe('timeout', () => {
   beforeEach(() => {
@@ -9,30 +14,20 @@ describe('timeout', () => {
   })
 
   it('no timeout', async () => {
-    async function heavyWork() {
-      await sleep(500)
-      return 'end'
-    }
-
-    const res = timeout(heavyWork(), 1_000).catch((e) => {
+    const res = timeout(heavyWork(50), 100).catch((e) => {
       console.log('invalid timeout', e)
     })
 
-    jest.advanceTimersByTime(500)
+    jest.advanceTimersByTime(50)
     await Promise.resolve()
 
     expect(await res).toBe('end')
   })
 
   it('throw timeout', async () => {
-    async function heavyWork() {
-      await sleep(5_000)
-      return 'end'
-    }
+    const res = timeout(heavyWork(200), 100).catch((e) => e.message)
 
-    const res = timeout(heavyWork(), 1_000).catch((e) => e.message)
-
-    jest.advanceTimersByTime(2_000)
+    jest.advanceTimersByTime(100)
     await Promise.resolve()
 
     expect(await res).toMatchInlineSnapshot(`"Timeout"`)
